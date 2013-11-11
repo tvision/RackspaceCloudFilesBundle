bca3170c3f90c22e76b1490b01d328cb7bbf18ba
claudio-dalicandro <claudio.dalicandro@gmail.com>
2013-11-11 13:34:56 +0100

    Added tests for dependency injection

--- DependencyInjection/TvisionRackspaceCloudFilesExtension.php
+++ DependencyInjection/TvisionRackspaceCloudFilesExtension.php
@@ -35,6 +35,5 @@ class TvisionRackspaceCloudFilesExtension extends Extension
         
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        
     }
 }
